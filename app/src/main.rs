b0255aa6a7c202c58eaa707a79d99de892c0a110
dev <dev@baras-app>
2025-12-17 11:36:03 -0800

    feat: initialize tauri+dioxus framework in app dir

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,10 @@
+mod app;
+
+use app::App;
+use dioxus::prelude::*;
+use dioxus_logger::tracing::Level;
+
 fn main() {
-    println!("Hello, world!");
+    dioxus_logger::init(Level::INFO).expect("failed to init logger");
+    launch(App);
 }
